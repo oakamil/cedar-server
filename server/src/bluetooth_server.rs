6a974741922fd4a3daaf758bf75148550ab75e0b
Omair Kamil <oakamil@gmail.com>
2026-01-10 06:04:46 +0000

    Complete implementation of bluetooth_server

--- server/src/bluetooth_server.rs
+++ server/src/bluetooth_server.rs
@@ -7,18 +7,27 @@ use bluer::{
     rfcomm::{Profile, Role, Stream},
     Session, Uuid,
 };
+use cedar_elements::{
+    cedar::{cedar_server::Cedar, EmptyMessage},
+    cedar_bt::{
+        cedar_bt_request::RequestOneof, cedar_bt_response::ResponseOneof,
+        CedarBtRequest, CedarBtResponse, RequestType, ResponseStatus,
+    },
+};
 use futures::StreamExt;
 use log::{debug, error, info, warn};
 use prost::{bytes::BytesMut, Message};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
-use cedar_elements::{
-  cedar_bt::{CedarBtRequest, CedarBtResponse, RequestType, ResponseStatus},
-};
+pub struct BluetoothServer<T> {
+    cedar: T,
+}
 
-pub struct BluetoothServer {}
+impl<T: Cedar> BluetoothServer<T> {
+    pub fn new(cedar: T) -> Self {
+        Self { cedar }
+    }
 
-impl BluetoothServer {
     pub async fn serve_requests(
         &mut self,
     ) -> Result<(), Box<dyn Error + 'static>> {
@@ -79,7 +88,8 @@ impl BluetoothServer {
             loop {
                 // Looping here in case we get multiple requests together
                 if let Some(resp) =
-                    self.process_incoming_data(&mut buffer).await {
+                    self.process_incoming_data(&mut buffer).await
+                {
                     if let Err(e) = stream.write_all(&resp).await {
                         warn!("Failed to send data to client: {}", e);
                     }
@@ -105,10 +115,13 @@ impl BluetoothServer {
                 // Clear the buffer since we don't know when the next properly
                 // formatted request starts
                 buffer.clear();
-                Some(CedarBtResponse {
+                Some(
+                    CedarBtResponse {
                         response_status: ResponseStatus::MalformedInput as i32,
                         response_oneof: None,
-                }.encode_length_delimited_to_vec())
+                    }
+                    .encode_length_delimited_to_vec(),
+                )
             }
         }
     }
@@ -154,10 +167,342 @@ impl BluetoothServer {
         }
     }
 
-    async fn process_parsed_request(&self, request: CedarBtRequest) -> CedarBtResponse {
-        // Placeholder
+    async fn process_parsed_request(
+        &self,
+        request: CedarBtRequest,
+    ) -> CedarBtResponse {
+        let req_type = match RequestType::try_from(request.request_type) {
+            Ok(t) => t,
+            Err(_) => {
+                return CedarBtResponse {
+                    response_status: ResponseStatus::UnknownRequest as i32,
+                    response_oneof: None,
+                }
+            }
+        };
+
+        match req_type {
+            RequestType::GetServerLog => {
+                if let Some(RequestOneof::ServerLogRequest(req)) =
+                    request.request_oneof
+                {
+                    match self
+                        .cedar
+                        .get_server_log(tonic::Request::new(req))
+                        .await
+                    {
+                        Ok(resp) => CedarBtResponse {
+                            response_status: ResponseStatus::Success as i32,
+                            response_oneof: Some(
+                                ResponseOneof::ServerLogResult(
+                                    resp.into_inner(),
+                                ),
+                            ),
+                        },
+                        Err(e) => Self::rpc_failed_response(e),
+                    }
+                } else {
+                    Self::invalid_req_response()
+                }
+            }
+            RequestType::UpdateFixedSettings => {
+                if let Some(RequestOneof::FixedSettings(req)) =
+                    request.request_oneof
+                {
+                    match self
+                        .cedar
+                        .update_fixed_settings(tonic::Request::new(req))
+                        .await
+                    {
+                        Ok(resp) => CedarBtResponse {
+                            response_status: ResponseStatus::Success as i32,
+                            response_oneof: Some(ResponseOneof::FixedSettings(
+                                resp.into_inner(),
+                            )),
+                        },
+                        Err(e) => Self::rpc_failed_response(e),
+                    }
+                } else {
+                    Self::invalid_req_response()
+                }
+            }
+            RequestType::ClearObserverLocation => {
+                match self
+                    .cedar
+                    .clear_observer_location(tonic::Request::new(
+                        EmptyMessage {},
+                    ))
+                    .await
+                {
+                    Ok(_) => CedarBtResponse {
+                        response_status: ResponseStatus::Success as i32,
+                        response_oneof: None,
+                    },
+                    Err(e) => Self::rpc_failed_response(e),
+                }
+            }
+            RequestType::UpdateOperationSettings => {
+                if let Some(RequestOneof::OperationSettings(req)) =
+                    request.request_oneof
+                {
+                    match self
+                        .cedar
+                        .update_operation_settings(tonic::Request::new(req))
+                        .await
+                    {
+                        Ok(resp) => CedarBtResponse {
+                            response_status: ResponseStatus::Success as i32,
+                            response_oneof: Some(
+                                ResponseOneof::OperationSettings(
+                                    resp.into_inner(),
+                                ),
+                            ),
+                        },
+                        Err(e) => Self::rpc_failed_response(e),
+                    }
+                } else {
+                    Self::invalid_req_response()
+                }
+            }
+            RequestType::UpdatePreferences => {
+                if let Some(RequestOneof::Preferences(req)) =
+                    request.request_oneof
+                {
+                    match self
+                        .cedar
+                        .update_preferences(tonic::Request::new(req))
+                        .await
+                    {
+                        Ok(resp) => CedarBtResponse {
+                            response_status: ResponseStatus::Success as i32,
+                            response_oneof: Some(ResponseOneof::Preferences(
+                                resp.into_inner(),
+                            )),
+                        },
+                        Err(e) => Self::rpc_failed_response(e),
+                    }
+                } else {
+                    Self::invalid_req_response()
+                }
+            }
+            RequestType::GetFrame => {
+                if let Some(RequestOneof::FrameRequest(req)) =
+                    request.request_oneof
+                {
+                    match self.cedar.get_frame(tonic::Request::new(req)).await {
+                        Ok(resp) => CedarBtResponse {
+                            response_status: ResponseStatus::Success as i32,
+                            response_oneof: Some(ResponseOneof::FrameResult(
+                                resp.into_inner(),
+                            )),
+                        },
+                        Err(e) => Self::rpc_failed_response(e),
+                    }
+                } else {
+                    Self::invalid_req_response()
+                }
+            }
+            RequestType::InitiateAction => {
+                if let Some(RequestOneof::ActionRequest(req)) =
+                    request.request_oneof
+                {
+                    match self
+                        .cedar
+                        .initiate_action(tonic::Request::new(req))
+                        .await
+                    {
+                        Ok(_) => CedarBtResponse {
+                            response_status: ResponseStatus::Success as i32,
+                            response_oneof: None,
+                        },
+                        Err(e) => Self::rpc_failed_response(e),
+                    }
+                } else {
+                    Self::invalid_req_response()
+                }
+            }
+            RequestType::QueryCatalogEntries => {
+                if let Some(RequestOneof::QueryCatalogRequest(req)) =
+                    request.request_oneof
+                {
+                    match self
+                        .cedar
+                        .query_catalog_entries(tonic::Request::new(req))
+                        .await
+                    {
+                        Ok(resp) => CedarBtResponse {
+                            response_status: ResponseStatus::Success as i32,
+                            response_oneof: Some(
+                                ResponseOneof::QueryCatalogResponse(
+                                    resp.into_inner(),
+                                ),
+                            ),
+                        },
+                        Err(e) => Self::rpc_failed_response(e),
+                    }
+                } else {
+                    Self::invalid_req_response()
+                }
+            }
+            RequestType::GetCatalogEntry => {
+                if let Some(RequestOneof::CatalogEntryKey(req)) =
+                    request.request_oneof
+                {
+                    match self
+                        .cedar
+                        .get_catalog_entry(tonic::Request::new(req))
+                        .await
+                    {
+                        Ok(resp) => CedarBtResponse {
+                            response_status: ResponseStatus::Success as i32,
+                            response_oneof: Some(ResponseOneof::CatalogEntry(
+                                resp.into_inner(),
+                            )),
+                        },
+                        Err(e) => Self::rpc_failed_response(e),
+                    }
+                } else {
+                    Self::invalid_req_response()
+                }
+            }
+            RequestType::GetCatalogDescriptions => {
+                match self
+                    .cedar
+                    .get_catalog_descriptions(tonic::Request::new(
+                        EmptyMessage {},
+                    ))
+                    .await
+                {
+                    Ok(resp) => CedarBtResponse {
+                        response_status: ResponseStatus::Success as i32,
+                        response_oneof: Some(
+                            ResponseOneof::CatalogDescriptionResponse(
+                                resp.into_inner(),
+                            ),
+                        ),
+                    },
+                    Err(e) => Self::rpc_failed_response(e),
+                }
+            }
+            RequestType::GetObjectTypes => {
+                match self
+                    .cedar
+                    .get_object_types(tonic::Request::new(EmptyMessage {}))
+                    .await
+                {
+                    Ok(resp) => CedarBtResponse {
+                        response_status: ResponseStatus::Success as i32,
+                        response_oneof: Some(
+                            ResponseOneof::ObjectTypeResponse(
+                                resp.into_inner(),
+                            ),
+                        ),
+                    },
+                    Err(e) => Self::rpc_failed_response(e),
+                }
+            }
+            RequestType::GetConstellations => {
+                match self
+                    .cedar
+                    .get_constellations(tonic::Request::new(EmptyMessage {}))
+                    .await
+                {
+                    Ok(resp) => CedarBtResponse {
+                        response_status: ResponseStatus::Success as i32,
+                        response_oneof: Some(
+                            ResponseOneof::ConstellationResponse(
+                                resp.into_inner(),
+                            ),
+                        ),
+                    },
+                    Err(e) => Self::rpc_failed_response(e),
+                }
+            }
+            RequestType::GetBluetoothName => {
+                match self
+                    .cedar
+                    .get_bluetooth_name(tonic::Request::new(EmptyMessage {}))
+                    .await
+                {
+                    Ok(resp) => CedarBtResponse {
+                        response_status: ResponseStatus::Success as i32,
+                        response_oneof: Some(
+                            ResponseOneof::GetBluetoothNameResponse(
+                                resp.into_inner(),
+                            ),
+                        ),
+                    },
+                    Err(e) => Self::rpc_failed_response(e),
+                }
+            }
+            RequestType::StartBonding => {
+                match self
+                    .cedar
+                    .start_bonding(tonic::Request::new(EmptyMessage {}))
+                    .await
+                {
+                    Ok(resp) => CedarBtResponse {
+                        response_status: ResponseStatus::Success as i32,
+                        response_oneof: Some(
+                            ResponseOneof::StartBondingResponse(
+                                resp.into_inner(),
+                            ),
+                        ),
+                    },
+                    Err(e) => Self::rpc_failed_response(e),
+                }
+            }
+            RequestType::GetBondedDevices => {
+                match self
+                    .cedar
+                    .get_bonded_devices(tonic::Request::new(EmptyMessage {}))
+                    .await
+                {
+                    Ok(resp) => CedarBtResponse {
+                        response_status: ResponseStatus::Success as i32,
+                        response_oneof: Some(
+                            ResponseOneof::GetBondedDevicesResponse(
+                                resp.into_inner(),
+                            ),
+                        ),
+                    },
+                    Err(e) => Self::rpc_failed_response(e),
+                }
+            }
+            RequestType::RemoveBond => {
+                if let Some(RequestOneof::RemoveBondRequest(req)) =
+                    request.request_oneof
+                {
+                    match self.cedar.remove_bond(tonic::Request::new(req)).await
+                    {
+                        Ok(_) => CedarBtResponse {
+                            response_status: ResponseStatus::Success as i32,
+                            response_oneof: None,
+                        },
+                        Err(e) => Self::rpc_failed_response(e),
+                    }
+                } else {
+                    Self::invalid_req_response()
+                }
+            }
+            _ => CedarBtResponse {
+                response_status: ResponseStatus::UnknownRequest as i32,
+                response_oneof: None,
+            },
+        }
+    }
+
+    fn rpc_failed_response(e: tonic::Status) -> CedarBtResponse {
+        warn!("RPC error: {:?}", e);
         CedarBtResponse {
-            response_status: ResponseStatus::Success as i32,
+            response_status: ResponseStatus::RpcFailed as i32,
+            response_oneof: None,
+        }
+    }
+
+    fn invalid_req_response() -> CedarBtResponse {
+        CedarBtResponse {
+            response_status: ResponseStatus::InvalidRequest as i32,
             response_oneof: None,
         }
     }
