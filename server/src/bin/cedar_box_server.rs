4d50b4d18dc3d0bd1ee97e0909fb9bc4c939c3ba
oakamil <oakamil@gmail.com>
2026-03-09 06:26:24 -0700

    Rust solver integration

--- server/src/bin/cedar_box_server.rs
+++ server/src/bin/cedar_box_server.rs
@@ -1,8 +1,14 @@
 // Copyright (c) 2024 Steven Rosenthal smr@dt3.org
 // See LICENSE file in root directory for license terms.
 
-use pico_args::Arguments;
+use std::{path::Path, sync::Arc};
 
+use cedar_solver::tetra3_solver::Tetra3Solver;
+use pico_args::Arguments;
+use tetra3::Tetra3;
+use tokio::sync::Mutex;
+
+use cedar_elements::solver_trait::SolverTrait;
 use cedar_server::cedar_server::server_main;
 
 fn main() {
@@ -12,5 +18,15 @@ fn main() {
          See LICENSE.md at https://github.com/smroid/cedar-server",
         /*flutter_app_path=*/"../cedar/cedar-aim/cedar_flutter/build/web",
         /*get_dependencies=*/
-        |_pargs: Arguments| { (None, None, None, None) });
+        |_pargs: Arguments| {
+            let db_path = Path::new("/home/cedar/cedar/data/default_database.npz");
+            let solver = Tetra3Solver::new(
+                    Tetra3::load_database(db_path)
+                        .expect("Failed to load Tetra3 database"),
+            );
+            let solver_arc: Arc<Mutex<dyn SolverTrait + Send + Sync>> = Arc::new(Mutex::new(solver));
+
+            (None, None, None, Some(solver_arc))
+        }
+    );
 }
